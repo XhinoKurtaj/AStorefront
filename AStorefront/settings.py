525a5bea71b76aaa7315fece6232e172135a4729
xhinoKurtaj <xhino_kurtaj@yahoo.com>
2022-03-08 22:43:16 +0100

    created application for store nad likes, also generated models for each table

--- AStorefront/settings.py
+++ AStorefront/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store',
+    'tags',
+    'likes'
 ]
 
 MIDDLEWARE = [
@@ -74,8 +77,11 @@ WSGI_APPLICATION = 'AStorefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'toor'
     }
 }
 
